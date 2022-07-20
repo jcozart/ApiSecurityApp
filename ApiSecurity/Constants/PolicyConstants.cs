7675b0dc8562770bbf2534ceafc11049efaea669
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-07-20 08:56:44 -0500

    Add project files.

---
+++ ApiSecurity/Constants/PolicyConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiSecurity.Constants;
+
+public class PolicyConstants
+{
+    public const string MustHaveEmployeeId = "MustHaveEmployeeId";
+    public const string MustBeTheOwner = "MustBeTheOwner";
+    public const string MustBeAVeteranEmployee = "MustBeAVeteranEmployee";
+}
---
+++ ApiSecurity/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ApiSecurity.Constants;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization(opts =>
+{
+    opts.AddPolicy(PolicyConstants.MustHaveEmployeeId, policy =>
+    {
+        policy.RequireClaim("employeeId");
+    });
+    opts.AddPolicy(PolicyConstants.MustBeTheOwner, policy =>
+    {
+        policy.RequireClaim("title", "Business Owner");
+    });
+    opts.AddPolicy(PolicyConstants.MustBeAVeteranEmployee, policy =>
+    {
+        policy.RequireClaim("employeeId", "E001", "E002", "E003");
+    });
+
+    opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+        };
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
